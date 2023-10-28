4de6cdbc06a6c7b777a7064f3726736c7b5f58fa
doenst1matter <1doesnt.matter1@mail.ru>
2023-10-28 20:52:31 +0300

    add routes and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import {Header} from "./components";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<h1>Главная</h1>}/>
+        <Route path="/documents" element={<h1>Документы</h1>}/>
+        <Route path="/contacts" element={<h1>Контакты</h1>}/>
+      </Routes>
     </div>
   );
 }
